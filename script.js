541399f10df04bdbff2543389b8b1a2b9dfcb506
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-22 19:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
+let form = document.getElementById("form1");
+function getFormvalue() 
+{
+    
+	alert(form.fname.value+" "+form.lname.value);
 
 }
